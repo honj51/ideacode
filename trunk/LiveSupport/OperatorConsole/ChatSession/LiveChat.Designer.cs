f0c47748bc939192173109ad7888367360cf7b63
miraclew <miraclew@f1c20352-1ea2-11de-b650-e715bd6d7cf1>
2009-04-24 04:25:42 +0000

    增加一个工具条，加入发送文件按钮

--- trunk/LiveSupport/OperatorConsole/ChatSession/LiveChat.Designer.cs
+++ trunk/LiveSupport/OperatorConsole/ChatSession/LiveChat.Designer.cs
@@ -29,29 +29,18 @@ namespace LiveSupport.OperatorConsole
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.wb = new System.Windows.Forms.WebBrowser();
             this.ctxMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toollblIsTyping = new System.Windows.Forms.ToolStripStatusLabel();
             this.tmrGetMsg = new System.Windows.Forms.Timer(this.components);
             this.txtMsg = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.wb = new System.Windows.Forms.WebBrowser();
             this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // wb
-            // 
-            this.wb.AllowNavigation = false;
-            this.wb.AllowWebBrowserDrop = false;
-            this.wb.ContextMenuStrip = this.ctxMenu;
-            this.wb.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.wb.IsWebBrowserContextMenuEnabled = false;
-            this.wb.Location = new System.Drawing.Point(0, 0);
-            this.wb.MinimumSize = new System.Drawing.Size(20, 18);
-            this.wb.Name = "wb";
-            this.wb.ScriptErrorsSuppressed = true;
-            this.wb.Size = new System.Drawing.Size(608, 330);
-            this.wb.TabIndex = 1;
-            this.wb.WebBrowserShortcutsEnabled = false;
             // 
             // ctxMenu
             // 
@@ -90,18 +79,62 @@ namespace LiveSupport.OperatorConsole
             this.txtMsg.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtMsg_KeyUp);
             this.txtMsg.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMsg_KeyPress);
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 262);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(608, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::LiveSupport.OperatorConsole.Properties.Resources.file_sent;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.ToolTipText = "·¢ËÍÎÄ¼þ";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // wb
+            // 
+            this.wb.AllowNavigation = false;
+            this.wb.AllowWebBrowserDrop = false;
+            this.wb.ContextMenuStrip = this.ctxMenu;
+            this.wb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wb.IsWebBrowserContextMenuEnabled = false;
+            this.wb.Location = new System.Drawing.Point(0, 0);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 18);
+            this.wb.Name = "wb";
+            this.wb.ScriptErrorsSuppressed = true;
+            this.wb.Size = new System.Drawing.Size(608, 262);
+            this.wb.TabIndex = 1;
+            this.wb.WebBrowserShortcutsEnabled = false;
+            // 
             // LiveChat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.wb);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.txtMsg);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.wb);
             this.Name = "LiveChat";
             this.Size = new System.Drawing.Size(608, 330);
             this.Load += new System.EventHandler(this.LiveChat_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,5 +148,8 @@ namespace LiveSupport.OperatorConsole
         private System.Windows.Forms.ContextMenuStrip ctxMenu;
         private System.Windows.Forms.Timer tmrGetMsg;
         private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
