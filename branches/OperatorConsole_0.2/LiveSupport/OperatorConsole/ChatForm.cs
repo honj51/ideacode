b3be365a0242ba14328f83485ad3f02f11f6b70a
wwj59@sina.com <wwj59@sina.com@f1c20352-1ea2-11de-b650-e715bd6d7cf1>
2009-05-05 01:11:24 +0000

    提交到新的分支 OperatorConsole_0.2

---
+++ branches/OperatorConsole_0.2/LiveSupport/OperatorConsole/ChatForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LiveSupport.OperatorConsole
+{
+    public partial class ChatForm : Form
+    {
+        public ChatForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
