156b2790568c8fba1f2a174a0337696146770be3
tao410396629 <tao410396629@qq.com>
2009-05-08 01:59:44 +0000

    该模块实现在LiveChat项目中, 提供接口包括： 1.	聊天会话管理/Chat Session Management 2.	聊天消息发送接收/Chat Message 3.	文件传输/File Transfer

---
+++ LiveSupport/LiveChat/App_Code/WebServices/ChatPageScriptMetod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+
+/// <summary>
+///ChatPageScriptMetod 的摘要说明
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+//若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。 
+// [System.Web.Script.Services.ScriptService]
+public class ChatPageScriptMetod : System.Web.Services.WebService
+{
+
+    public ChatPageScriptMetod()
+    {
+
+        //如果使用设计的组件，请取消注释以下行 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+}
+
