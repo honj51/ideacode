1f4eb490ef78211371558360e120b425174e5c70
583674718 <583674718@qq.com>
2011-05-08 23:06:52 +0000

    新增房产

--- Project/WYGL2/ZiYuan/fcgl.aspx.cs
+++ Project/WYGL2/ZiYuan/fcgl.aspx.cs
@@ -14,17 +14,21 @@ public partial class ZiYuan_fcgl : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string action = Request.QueryString["action"];
+        string action = Request.Params["action"];
         if (String.IsNullOrEmpty(action)) return;
+        Response.ContentType = "application/json";
 
-        if (action == "load_data")
+        if (action == "list")
         {
-            SqlDataReader r = DBHelper.GetReader("select * from sq8szxlx.gyy_fc_lb  order by 工业园名称,房产类型,房号 asc");
-            Console.WriteLine("111111");
-            Console.WriteLine("2222");
-            Response.ContentType = "application/json";
-            Response.Write(Json.ToJson(r));
-            Response.End();
+            SqlDataReader r = DBHelper.GetReader("select * from sq8szxlx.gyy_fc_lb  order by 工业园名称,房产类型,房号 asc");          
+            Response.Write(Json.ToJson(r));         
+        }else if (action == "add")
+        {
+            string sql = SqlBuilder.NameValueToSql(Request.Form, "sq8szxlx.gyy_fc_lb", "id", true);
+            DBHelper.ExecuteSql(sql);
+            Response.Write("{success: true}");  
         }
+
+        Response.End();
     }
 }
